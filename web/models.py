9aaacdf17afc06fcd67d508265562f5017baa0e0
unknown <rezaei-nim@nima-int.NIOEC.co.ir.local>
2023-06-19 08:54:19 +0330

    initialcommit2

--- web/models.py
+++ web/models.py
@@ -1,3 +1,11 @@
+from __future__ import unicode_literals
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Create your models here.now
+
+class Expense(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User)
