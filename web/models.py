23f881a8e602ede6bfb1b1851bb5e5285765101c
unknown <rezaei-nim@nima-int.NIOEC.co.ir.local>
2023-07-04 11:14:12 +0330

    dakhlo kharj

--- web/models.py
+++ web/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 # Create your models here.now
 class Token(models.Model):
     user = models.OneToOneField(User , on_delete=models.CASCADE)
-    Token = models.CharField(max_length=48)
+    token = models.CharField(max_length=48, blank=True , null=True)
     def __str__(self):
         return "{}_token".format(self.user)
 
--- web/views.py
+++ web/views.py
@@ -2,22 +2,39 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from json import JSONEncoder
 from django.views.decorators.csrf import csrf_exempt
-from web.models import User , Token , income ,Expense
+from web.models import User ,income ,Expense , Token
 from datetime import datetime
 # Create your views here.
 @csrf_exempt
-def submit_expense(request):
-    """user submit an expense"""
-    if request.method == 'POST':
-         this_Token = request.POST['token']
-    this_user = User.objects.filter(Token__Token = this_Token).get()
-    now = datetime.now()
-    Expense.objects.create(User=this_user, amount=request.post['amount'],
-            text = request.post['text'],date =now)
-    print ("I'm in submit expense")
-    if request.method == 'POST':
-           print(request.POST)
+def submit_income(request):
+    """user submit an income"""
+    this_token = request.POST['token']
+    print (this_token)
+    this_user = Token.objects.get(token = this_token).user
+    print(this_user)
+    if 'date' not in request.POST:
+        date = datetime.now()
+    income.objects.create(user=this_user, amount=request.POST['Amount'],
+        text = request.POST['Text'],date = date)
+        
     return JsonResponse({
         'status' : 'ok', 
     }, encoder= JSONEncoder)
+   
 
+@csrf_exempt
+def submit_expense(request):
+    """user submit an expense"""
+    this_token = request.POST['token']
+    print(this_token)
+    this_user =Token.objects.get(token = this_token).user
+    print(this_user)
+    
+    if 'date' not in request.POST:
+        date = datetime.now()
+    Expense.objects.create(user=this_user, amount=request.POST['Amount'],
+        text = request.POST['Text'],date = date)
+        
+    return JsonResponse({
+        'status' : 'ok', 
+    }, encoder= JSONEncoder)
